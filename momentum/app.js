ac85de5e2d28041691bb681b8a15cee444ebd17b
Chanran kim <126753671+isliese@users.noreply.github.com>
2024-11-02 19:46:18 -0700

    Add files via upload

---
+++ momentum/app.js
@@ -0,0 +1,24 @@
+// alert("hi"); // function임 
+// console.log(54545454);
+
+// const는 상수임. 값을 바꿀 수 없음 (value 보호 가능)
+const a = 5;
+const b = 2;
+
+console.log('6+2');
+console.log("6*2");
+console.log("6/2");
+console.log("hello " + "Isla");
+
+
+// let은 상수가 아님. 값을 바꿀 수 없음
+const myName = "Isla";
+
+myName = "Charnan";
+console.log("your name is " + myName); 
+
+// var는 let이나 const가 없었을 떄 자주 쓰던 거
+// var는 내가 원할 때 어디서든 변수를 업데이트 할 수 있음
+
+// undefined = 변수는 선언했지만 값을 할당하지는 않음
+// null = 변수에 null(값이 없다)이 할당된다 즉, 정의됨
