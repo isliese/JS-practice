cd6c87aae033c59f39a5f846aa78ce827c671441
Chanran kim <126753671+isliese@users.noreply.github.com>
2024-11-04 22:46:42 -0800

    12

--- momentum/app.js
+++ momentum/app.js
@@ -1,6 +1,6 @@
 // 자바스크립트는 HTML을  불러오는 것이다.
 // JS 에서 htnl을 불러오는 것뿐만 아니라 변경할 수도 있다.
 
-const title = document.getElementById("title");
+const title = document.getElementById(title);
 
-console.dir(title);
+title.innerText = "Got you";
